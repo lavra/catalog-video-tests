738a7496feb75c214d810caf724706826ad46fc1
Luiz Anselmo <lavra2021@gmail.com>
2022-07-30 03:04:12 -0300

    Criar teste de unidade do usecase de criação de categoria

---
+++ tests/Unit/UseCase/Category/CreateCategoryUseCaseUnitTest.php
@@ -0,0 +1,38 @@
+<?php 
+
+namespace Tests\Unit\UseCase\Category;
+
+use Mockery;
+use stdClass;
+use PHPUnit\Framework\TestCase;
+use Core\Domain\Entity\Category;
+use PhpParser\Builder\InterfaceTest;
+use Core\UseCase\Category\CreateCategoryUseCase;
+use Core\Domain\Repository\CategoryRepositoryInterface;
+
+class CreateCategoryUseCaseUnitTest extends TestCase
+{
+    public function testCreateNewCategory()
+    {
+        $categoryId = '1a2b';
+        $categoryName = 'Name';
+        $categoryDescription = 'Description';
+        $categoryIsActive = true;
+        $categoryCreatedAt = '2023-01-01 00:00:00';
+
+
+        $this->mockEntity = Mockery::mock(Category::class, [
+            $categoryId,
+            $categoryName,
+            $categoryDescription,
+            $categoryIsActive,
+            $categoryCreatedAt
+        ]);
+
+        $this->mockRepository = Mockery::mock(stdClass::class, CategoryRepositoryInterface::class);
+        $this->mockRepository->shouldReceive('insert')->andReturn($this->mockEntity);
+
+        $useCase =  new CreateCategoryUseCase($this->mockRepository);
+        $useCase->execute();
+    }
+}
